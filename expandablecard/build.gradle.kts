d87d22b0281cc056f452dc96c91a923decf873ce
Ilham Hadisyah <ilham.hadisyah@akarinti.tech>
2025-01-09 15:41:09 +0700

    fix jitpack config

--- expandablecard/build.gradle.kts
+++ expandablecard/build.gradle.kts
@@ -81,7 +81,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.ilhamhadisyahramadhani"
             artifactId = "expandablecard"
-            version = "1.6"
+            version = "1.4.2"
 //            from(components["release"])
             afterEvaluate{
                 from(components["release"])
