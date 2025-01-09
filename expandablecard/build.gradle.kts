681d972a4ca5a276512fa0966504af749ae426db
Ilham Hadisyah <ilham.hadisyah@akarinti.tech>
2025-01-09 16:22:38 +0700

    fix jitpack config

--- expandablecard/build.gradle.kts
+++ expandablecard/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     id("maven-publish")
 
@@ -10,11 +10,11 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.expandablecard"
+//        applicationId = "com.example.expandablecard"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+//        versionCode = 1
+//        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
